38a9f43980591f0c6faaf680914e5ea8692bc629
Cipher Boy <GAS_2005__@hotmail.com>
2025-08-04 23:47:23 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,19 +4,21 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  subscription: { 
-    type: Boolean, 
-    default: false 
-  },
+  subscription: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
 
 // Hash password before saving
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
